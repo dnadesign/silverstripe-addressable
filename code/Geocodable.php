fd6cfd015110e67a80ee0e2c5fc516b19ff18709
John Milmine <john.milmine@dna.co.nz>
2014-04-16 14:36:08 +1200

    changed to 2

--- code/Geocodable.php
+++ code/Geocodable.php
@@ -13,7 +13,7 @@ class Geocodable extends DataExtension {
 	);
 
 	public function onBeforeWrite() {
-		if (!$this->owner->isAddressChanged()) return;
+		if (!$this->owner->isAddressChanged(2)) return;
 
 		$address = $this->owner->getFullAddress();
 		$region  = strtolower($this->owner->Country);
